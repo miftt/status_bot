703bd46d5f61ab0e1c45e8b372cb38c51f8e5d4e
miftt <miftahfauzi012@gmail.com>
2024-01-18 22:25:32 +0700

    fix: auth for itemlist

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,4 +7,4 @@ export function mainMiddleware(request: NextRequest) {
     return res;
 }
 
-export default withAuth(mainMiddleware, ['/', '/dashboard', '/profile', '/login', '/register']);
+export default withAuth(mainMiddleware, ['/', '/dashboard', '/profile', '/login', '/register', '/itemlist']);
