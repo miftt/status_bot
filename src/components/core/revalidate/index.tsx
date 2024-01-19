3666fab47d7523e22842855a13eea59c9c833f25
miftt <miftahfauzi012@gmail.com>
2024-01-19 13:30:12 +0700

    fix loading

--- src/components/core/revalidate/index.tsx
+++ src/components/core/revalidate/index.tsx
@@ -7,47 +7,49 @@ import { Toaster, toast } from "sonner";
 
 const RevalidatePage = () => {
   const [loading, setLoading] = useState(false);
-  const [loadingText, setLoadingText] = useState('Refreshing');
+  const [loadingDots, setLoadingDots] = useState(0);
   const router = useRouter();
   const refresh = () => router.refresh();
 
-
   useEffect(() => {
     const intervalId = setInterval(() => {
-        setLoadingText((prevLoadingText) => {
-            return prevLoadingText === 'Refreshing Data...' ? 'Refresh' : `${prevLoadingText}.`;
-        });
+      setLoadingDots((prevLoadingDots) => {
+        return prevLoadingDots >= 3 ? 0 : prevLoadingDots + 1;
+      });
     }, 1000);
 
     return () => clearInterval(intervalId); // Clear interval on unmount
-}, []);
+  }, []);
 
   const revalidate = async () => {
-    setLoading(true)
+    setLoading(true);
     const res = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/revalidate?tag=bot&secret=mifuzigantengbanget`,{
       method: 'POST',
     });
     if (res.status !== 200) {
       setLoading(false);
       toast.error(`Revalidate Failed`);
-      return 
-  } else {
-      const response = await res.data
+      return;
+    } else {
+      const response = await res.data;
       if(response.revalidate){
-          toast.success(`Revalidate Success`);
+        toast.success(`Revalidate Success`);
       };
       refresh();
       setLoading(false);
-  }   
+    }   
   }
-    return ( 
-        <div className="mt-2 flex flex-col gap-4">
-          <Toaster position="top-center" richColors />
-            <button className="btn btn-sm btn-primary" onClick={() => revalidate()}>
-            {loading ? loadingText : 'Refresh Data'}
-            </button>
-        </div>
-     );
+
+  const loadingText = `Refreshing${'.'.repeat(loadingDots)}`;
+
+  return (
+    <div className="mt-2 flex flex-col gap-4">
+      <Toaster position="top-center" richColors/>
+      <button className="btn btn-sm btn-primary" onClick={() => revalidate()}>
+        {loading ? loadingText : 'Refresh Data'}
+      </button>
+    </div>
+  );
 }
- 
+
 export default RevalidatePage;
