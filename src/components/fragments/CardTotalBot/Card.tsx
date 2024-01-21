7aeb978a58a29692904789d6dcc7526a04f27762
miftt <miftahfauzi012@gmail.com>
2024-01-21 11:00:30 +0700

    fix: ui

--- src/components/fragments/CardTotalBot/Card.tsx
+++ src/components/fragments/CardTotalBot/Card.tsx
@@ -9,7 +9,7 @@ const CardTotalBot: React.FC<cardTotalBotProps> = ({
     total
 }) => {
     return ( 
-        <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-4 border border-gray-400 rounded-xl">
+        <div className="w-full mb-4 border border-gray-400 rounded-xl">
             <div className="card-body">
                 <h5 className="card-title">Total {info} Bot</h5>
                 <p className="card-text">{total}</p>
