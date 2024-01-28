a95dbc0ad527560c5b7b203402ce7c5c31b013fc
miftt <miftahfauzi012@gmail.com>
2024-01-28 17:42:43 +0700

    change to env

--- src/lib/supabase/init.ts
+++ src/lib/supabase/init.ts
@@ -1,2 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-export const client = createClient('https://injscivczsotoqqwbapn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluanNjaXZjenNvdG9xcXdiYXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1NDUyODUsImV4cCI6MjAyMDEyMTI4NX0.eeMdd5GbASBj5Fhl7Me7l7F96yaPJrLbWkxxLMcjFss')
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const client = createClient(supabaseUrl as string, supabaseAnonKey as string)
